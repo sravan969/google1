9048f624990a687cd3232fb56bd2639e5ba20079
sravan969 <sravan.mghs@gmail.com>
2019-07-29 20:40:08 +0530

    4th commit

---
+++ src/test/java/google/googletest.java
@@ -0,0 +1,19 @@
+package google;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Test;
+
+public class googletest {
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
